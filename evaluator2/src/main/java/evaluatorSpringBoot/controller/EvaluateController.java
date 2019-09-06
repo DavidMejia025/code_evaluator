05de8db5c695d15814e492298d7f8d0f298eb37b
david <davidndy@gmail.com>
2019-09-06 15:51:11 -0500

    fix docker client issues and conect it to build a docker image

--- evaluator2/src/main/java/evaluatorSpringBoot/controller/EvaluateController.java
+++ evaluator2/src/main/java/evaluatorSpringBoot/controller/EvaluateController.java
@@ -1,6 +1,7 @@
 package evaluatorSpringBoot.controller;
 
 
+import java.io.File;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateContainerCmd;
+import com.github.dockerjava.api.command.InspectImageResponse;
 import com.github.dockerjava.api.model.Container;
-import com.github.dockerjava.api.model.Info;
 import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.command.BuildImageResultCallback;
 
 import evaluatorSpringBoot.Code;
 
@@ -27,15 +30,23 @@ public class EvaluateController {
   @PostMapping(value = "/cheers", headers="Accept=application/json", consumes = "application/JSON")
   public Code postEvaluate(@RequestBody String name) {
     dockerClient = DockerClientBuilder.getInstance().build();
-    List<Container> containers = dockerClient.listContainersCmd().exec();
-    System.out.println(containers.toString());
-   // dockerClient.buildImageCmd(new File("src/test/resources", directory)).withNoCache(true)
-    //.exec(new BuildImageResultCallback()).awaitImageId();
-    Info info = dockerClient.infoCmd().exec();
-    System.out.print(info); 
     
-    System.out.println(name.getClass().getName());
-    System.out.println(name);
+    
+    String imageId = dockerClient.buildImageCmd(new File("src/test/resources")).withNoCache(true)
+    .exec(new BuildImageResultCallback()).awaitImageId();
+    
+    //Info info = dockerClient.infoCmd().exec();
+    //System.out.print(info); 
+    InspectImageResponse image = dockerClient.inspectImageCmd(imageId).exec();
+    String newContainer = dockerClient.createContainerCmd(imageId).exec().getId();
+
+    dockerClient.startContainerCmd(newContainer).exec();
+    
+    List<Container> containers = dockerClient.listContainersCmd().exec();
+    
+    System.out.println("start");
+    System.out.println(containers.toString());
+
     Code newCode =  new Code(counter.incrementAndGet(),
                          String.format(template,name));
 
