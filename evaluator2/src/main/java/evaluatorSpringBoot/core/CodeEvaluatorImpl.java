061db53b13bb512e1e5f01611f9505001a3b7372
Germán Escobar <german.escobarc@gmail.com>
2019-09-25 12:32:57 -0500

    Fix paths

--- evaluator2/src/main/java/evaluatorSpringBoot/core/CodeEvaluatorImpl.java
+++ evaluator2/src/main/java/evaluatorSpringBoot/core/CodeEvaluatorImpl.java
@@ -27,41 +27,41 @@ import evaluatorSpringBoot.services.docker.MyDockerClientImpl;
 @Service
 public class CodeEvaluatorImpl implements CodeEvaluator {
   private ApplicationContext context         = new AnnotationConfigApplicationContext(Config.class);
-	private final String       basePath        = "submissions/";
-	private final String       stdoutFilePath  = "submissions/documents/stdout_test.rb";
+	private final String       basePath        = "/codeEvaluator";
+	private final String       stdoutFilePath  = "/codeEvaluator/submissions/documents/stdout_test.rb";
 	private SubmissionDao      submissionDAO;
 	private ResponseDao        responseDAO;
 	private String             submissionInput;
 	private String             testPath;
 	private LogSystem          logs;
-	
+
 	public CodeEvaluatorImpl(String submissionInput) {
 	  this.submissionInput = submissionInput;
 	  submissionDAO        = context.getBean("SubmissionDaoBean",SubmissionDao.class);
 	  responseDAO          = context.getBean("ResponseDaoBean", ResponseDao.class);
 	  logs                 = context.getBean("LogsBean",LogSystem.class);
-	  
+
 	  //this.submissionDAO   = FactoryDao.createSubmission(); // string injector can avoid this line of code
 	  //this.responseDAO     = FactoryDao.createResponse();
   }
-	
+
 	@Override
 	public Response runEval() throws IOException{
 	  logs.addLog("Submision received with input:" + this.submissionInput);
 		String code = parseJson(this.submissionInput);
 
 		Submission newSubmission = new Submission(code);
-		submissionDAO.create(newSubmission);
-		
+		// submissionDAO.create(newSubmission);
+
 		prepare(newSubmission);
-		
+
 		String result = runTest(newSubmission);
-		
-		cleanTest(newSubmission);
+
+		// cleanTest(newSubmission);
 
 	  Response newResponse = new Response(newSubmission.getSubmissionId(), result, 200);
-	  responseDAO.create(newResponse);
-	  
+	  //responseDAO.create(newResponse);
+
     return newResponse;
     }
 
@@ -70,101 +70,102 @@ public class CodeEvaluatorImpl implements CodeEvaluator {
 		createTestFile(newSubmission);
 		copyStdoutFile(newSubmission);
 	}
-	
+
 	private String runTest(Submission newSubmission) {
 		MyDockerClientImpl dockerClient = new MyDockerClientImpl();
-		
+
 		dockerClient.createContainer(newSubmission);
 		dockerClient.startContainer(dockerClient.getContainerId());
-		
+
 		String dockerLogs = dockerClient.getLogs(dockerClient.getContainerId());
-		
+
 		createResultFile(newSubmission, dockerLogs);;
-		
+
 		return dockerLogs;
 	}
-	
+
 	private void cleanTest(Submission newSubmission) {
 		String path = testPath;
 		File dir    = new File(path);
-		
+
 		deleteDirectory(dir);
 	}
-	
+
 	private String parseJson(String json) {
 		JSONObject obj = new JSONObject(json);
-		
+
 		String jsonParsed = obj.getString("code");
-		
+
 		return jsonParsed;
 	}
-	
-	private void createSubmissionFolder(Submission newSubmission) { 
+
+	private void createSubmissionFolder(Submission newSubmission) {
 	  testPath = basePath + Integer.toString(newSubmission.getSubmissionId());
 		File newFolder = new File(testPath);
-    
+
     boolean created =  newFolder.mkdir();
 	}
-	
+
 	public void createTestFile(Submission newSubmission) {
-		String fileUrl = testPath + "/" + "user_source_code.rb";  
+		String fileUrl = testPath + "/" + "user_source_code.rb";
 		File new_file  = new File(fileUrl);
 
 		String code = newSubmission.getCode();
-		
+
     String fileData = "def test;" + code + ";"+ "end";
     generateFOS(new_file, fileData);
   }
-    
+
   public void createResultFile(Submission newSubmission, String body) {
-  	String resultUrl = testPath + "/" + "result.txt"; 
+  	String resultUrl = testPath + "/" + "result.txt";
   	File new_file    = new File(resultUrl);
-      
-  	String fileData = "Result of the code evaluation is \n" + body + "\n"; 
+
+  	String fileData = "Result of the code evaluation is \n" + body + "\n";
     generateFOS(new_file, fileData);
   }
-	
+
 	public String readTestFile(Submission newSubmission){
-		String resultUrl = testPath + "/" + "result.txt"; 
+		String resultUrl = testPath + "/" + "result.txt";
 		String result    = "";
 	  String line      = null;
-	    
+
 		try {
 	    FileReader fileReader = new FileReader(resultUrl);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
-	      
+
       while((line = bufferedReader.readLine()) != null) {
           result += "-----" + line + "-----";
-      }  
-	        
-	      bufferedReader.close();  	      
+      }
+
+	      bufferedReader.close();
 	    }
 	    catch(FileNotFoundException ex) {
-            System.out.println("Unable to open file '" + resultUrl + "'");                
+            System.out.println("Unable to open file '" + resultUrl + "'");
       }
       catch(IOException ex) {
-          System.out.println("Error reading file '" + resultUrl + "'");                  
+          System.out.println("Error reading file '" + resultUrl + "'");
       }
-		
+
 		return result;
 	}
-	
+
 	private void copyStdoutFile(Submission newSubmission) throws IOException{
 		String from = stdoutFilePath;
 		String to   = testPath + "/" + "stdout_test.rb";
-		
+
 		Path srcFilePath  = Paths.get(from);
     Path destFilePath = Paths.get(to);
-    
+
     Files.copy(srcFilePath.toFile(), destFilePath.toFile());
-    
+		System.out.println("!!!!!!!!!!!!!!!! " + destFilePath);
+
     newSubmission.setStdoutPath("/submissions/" + Integer.toString(newSubmission.getSubmissionId()) + "/" + "stdout_test.rb"); //Think twice this route
 	}
-	
+
 	public static boolean deleteDirectory(File dir) {
 		if (dir.isDirectory()) {
 			File[] children = dir.listFiles();
-			
+
 			for (int i = 0; i < children.length; i++) {
 				deleteDirectory(children[i]);
 			}
@@ -172,11 +173,11 @@ public class CodeEvaluatorImpl implements CodeEvaluator {
 
 		return dir.delete();
 	}
-	
+
 	private void generateFOS(File new_file, String fileData) {
 		try {
 			FileOutputStream fos = new FileOutputStream(new_file);
-			
+
 			fos.write(fileData.getBytes());
 			fos.flush();
 			fos.close();
--- evaluator2/src/main/java/evaluatorSpringBoot/services/docker/MyDockerClientImpl.java
+++ evaluator2/src/main/java/evaluatorSpringBoot/services/docker/MyDockerClientImpl.java
@@ -26,42 +26,40 @@ import evaluatorSpringBoot.services.LogSystem;
 
 public class MyDockerClientImpl implements MyDockerClient {
   private ApplicationContext context  = new AnnotationConfigApplicationContext(Config.class);
-	private final String volume_path    = "/home/deif/Dropbox/Elite/projects/code_evaluator/evaluator2";
-	private final String dockerFilePath = "src/main/resources/docker/Dockerfile";
-	
+	private final String volume_path    = "/codeEvaluator";
+	private final String dockerFilePath = "/codeEvaluator/src/main/resources/docker/Dockerfile";
+
 	private String       imageId;
-	private String       containerId; 
+	private String       containerId;
 	private DockerClient client;
 	private LogSystem    logs;
-	
+
 	public MyDockerClientImpl() {
 		this.client = DockerClientBuilder.getInstance().build();
 		logs        = context.getBean(LogSystem.class);
 	}
-	
+
 	public DockerClient buildClient() {
-		return DockerClientBuilder.getInstance().build(); 
+		return DockerClientBuilder.getInstance().build();
 	}
-	
+
 	public DockerClient getClient() {
 		return this.client;
 	}
-	
+
 	public String getContainerId() {
 		return this.containerId;
 	}
-	
+
 	@Override
 	public void createContainer(Submission newSubmission) {
 		this.imageId = getImageId(client, "build");
-		
-		Volume volume1    = new Volume("/app");
-    String targetFile = "/app" + newSubmission.getStdoutPath(); 
 
-    CreateContainerResponse container = this.client.createContainerCmd("121862ceb25f")
-      .withVolumes(volume1)
-      .withBinds(new Bind(volume_path, volume1))
-      .withCmd("ruby", targetFile)
+    String targetFile = "/app" + newSubmission.getStdoutPath();
+
+    CreateContainerResponse container = this.client.createContainerCmd(this.imageId)
+      .withBinds(new Bind(volume_path, new Volume("/app")))
+      .withCmd("bin/ls", "/app")
       .exec();
 
 		this.containerId = container.getId();
@@ -75,36 +73,37 @@ public class MyDockerClientImpl implements MyDockerClient {
 	@Override
 	public void stopContainer(String containerId) {
 		//Not implemented yet
-		
+
 	}
 
 	@Override
 	public void removeContainer(String containerId) {
 	//Not implemented yet
-		
+
 	}
 
 	@Override
 	public String getLogs(String containerId){
 		FrameReaderITestCallback collectFramesCallback = new FrameReaderITestCallback();
-	    
+
 	  try {
 			this.client.logContainerCmd(containerId)
 				  .withFollowStream(true)
 					.withStdOut(true)
+					.withStdErr(true)
 					.withTailAll()
 					.exec(collectFramesCallback).awaitCompletion();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-	  
+
     String result = collectFramesCallback.frames.toString().replaceAll("STDOUT: ", "");
 
     logs.addLog("Get evaluator logs:  " + result);
-    
+
     return result;
 	}
-	
+
 	public static class FrameReaderITestCallback extends LogContainerResultCallback {
     public List<Frame> frames = new ArrayList<>();
 
@@ -114,41 +113,41 @@ public class MyDockerClientImpl implements MyDockerClient {
       super.onNext(item);
     }
   }
-	
+
 	@Override
 	public String getImageId(DockerClient client, String method) {
 		String imageName = "ruby";
-		
+
 		if (method == "build") {
 			imageId = buildImage(client, dockerFilePath);
 		} else {
 			if (findImageId(this.client, "rubyx") == ""){
 				pullImage(this.client, "ruby:2.5.5");
 		  }
-			
+
 			imageId = findImageId(this.client, "ruby");
 		}
-		
+
 		logs.addLog("Get image ids:" + imageId);
 
 		return imageId;
 	}
-	
+
 	public void pullImage(DockerClient client, String name) {
 		client.pullImageCmd(name).exec(new PullImageResultCallback());
 	}
-	
+
 	private String findImageId(DockerClient client, String name) {
 		List<Image> images  = client.listImagesCmd().exec();
 		Pattern regexImage  = Pattern.compile(name);
 		String imageIdFound = "";
-	
+
 		for(int i = 0; i < images.size(); i++){
 			String[] repoTags  = images.get(i).getRepoTags();
-			
+
 			for (int j = 1; j <= repoTags.length; j++) {
 				Matcher m = regexImage.matcher(repoTags[j-1]);
-				
+
 				if (m.find()) {
 					imageIdFound = images.get(i).getId().replaceAll("sha256:", "");
 					j = repoTags.length + 1;
@@ -159,13 +158,13 @@ public class MyDockerClientImpl implements MyDockerClient {
 
     return imageIdFound;
 	}
-	
+
 	public String buildImage(DockerClient client, String dockerFilePath) {
 		String imageId = client.buildImageCmd(new File(dockerFilePath))
   		.withNoCache(true)
   	  .exec(new BuildImageResultCallback())
   	  .awaitImageId();
-		
+
 		return imageId;
 	}
 }
